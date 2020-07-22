e8d5a5c6ecaf64a8cef817c823e3410f952e5f14
Alejandro Tardín <alejandro.tardin@liferay.com>
2020-07-22 17:02:34 +0200

    Creating Pokémon portlet

---
+++ src/main/java/com/liferay/talks/item/selector/internal/pokemon/PokemonService.java
@@ -0,0 +1,126 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.talks.item.selector.internal.pokemon;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.util.Http;
+
+import java.io.IOException;
+
+import java.net.HttpURLConnection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alejandro Tardín
+ */
+@Component(service = PokemonService.class)
+public class PokemonService {
+
+	public int getPokemonCount() {
+		return 150;
+	}
+
+	public List<Pokemon> getPokemons(int start, int end)
+		throws PortalException {
+
+		try {
+			JSONObject jsonObject = _fetchJSONObject(
+				"https://pokeapi.co/api/v2/pokemon?offset=" + start);
+
+			JSONArray resultsJSONArray = jsonObject.getJSONArray("results");
+
+			List<Pokemon> pokemons = new ArrayList<>();
+
+			for (int i = 0; i < resultsJSONArray.length(); i++) {
+				JSONObject pokemonInitialJSONObject =
+					resultsJSONArray.getJSONObject(i);
+
+				JSONObject pokemonJSONObject = _fetchJSONObject(
+					pokemonInitialJSONObject.getString("url"));
+
+				pokemons.add(
+					new Pokemon() {
+
+						@Override
+						public String getImageURL() {
+							return pokemonJSONObject.getJSONObject(
+								"sprites"
+							).getString(
+								"front_default"
+							);
+						}
+
+						@Override
+						public String getName() {
+							return pokemonJSONObject.getString("name");
+						}
+
+						@Override
+						public String getType() {
+							return pokemonJSONObject.getJSONArray(
+								"types"
+							).getJSONObject(
+								0
+							).getJSONObject(
+								"type"
+							).getString(
+								"name"
+							);
+						}
+
+					});
+			}
+
+			return pokemons;
+		}
+		catch (IOException ioException) {
+			throw new PortalException(ioException);
+		}
+	}
+
+	private JSONObject _fetchJSONObject(String url)
+		throws IOException, PortalException {
+
+		Http.Options options = new Http.Options();
+
+		options.setLocation(url);
+
+		String responseJSON = _http.URLtoString(options);
+
+		Http.Response response = options.getResponse();
+
+		if (response.getResponseCode() != HttpURLConnection.HTTP_OK) {
+			throw new PortalException(
+				StringBundler.concat(
+					"Response code ", response.getResponseCode(), ": ",
+					responseJSON));
+		}
+
+		return JSONFactoryUtil.createJSONObject(responseJSON);
+	}
+
+	@Reference
+	private Http _http;
+
+}
---
+++ src/main/java/com/liferay/talks/item/selector/internal/portlet/PokemonPortlet.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.talks.item.selector.internal.portlet;
+
+import com.liferay.item.selector.ItemSelector;
+import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactoryUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.talks.item.selector.internal.constants.PokemonPortletKeys;
+import com.liferay.talks.item.selector.internal.item.selector.PokemonItemSelectorCriterion;
+
+import java.io.IOException;
+
+import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.PortletURL;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alejandro Tardín
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.application-type=widget",
+		"com.liferay.portlet.css-class-wrapper=portlet-pokemon",
+		"com.liferay.portlet.display-category=category.collaboration",
+		"com.liferay.portlet.header-portlet-css=/main.css",
+		"com.liferay.portlet.icon=/pokeball.png",
+		"com.liferay.portlet.layout-cacheable=true",
+		"com.liferay.portlet.use-default-template=true",
+		"javax.portlet.display-name=Pokémon",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.always-display-default-configuration-icons=true",
+		"javax.portlet.init-param.always-send-redirect=true",
+		"javax.portlet.init-param.template-path=/META-INF/resources/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + PokemonPortletKeys.POKEMON,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=guest,power-user,user"
+	},
+	service = Portlet.class
+)
+public class PokemonPortlet extends MVCPortlet {
+
+	@Override
+	public void render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws IOException, PortletException {
+
+		PortletURL itemSelectorURL = _itemSelector.getItemSelectorURL(
+			RequestBackedPortletURLFactoryUtil.create(renderRequest),
+			_portal.getPortletNamespace(_portal.getPortletId(renderRequest)) +
+				"selectPokemon",
+			new PokemonItemSelectorCriterion());
+
+		renderRequest.setAttribute(
+			"itemSelectorURL", itemSelectorURL.toString());
+
+		super.render(renderRequest, renderResponse);
+	}
+
+	@Reference
+	private ItemSelector _itemSelector;
+
+	@Reference
+	private Portal _portal;
+
+}
