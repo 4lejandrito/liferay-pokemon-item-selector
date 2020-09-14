c256d145bafd25e70d9456f009be5cf73586352a
Alejandro Tardín <alejandro.tardin@liferay.com>
2020-09-14 18:27:24 +0200

    Step 5: Creating a React component to select and display a Pokémon
    We need to use the latest version of kernel for the module to resolve.

--- src/main/resources/META-INF/resources/lib/index.es.js
+++ src/main/resources/META-INF/resources/lib/index.es.js
@@ -12,9 +12,91 @@
  * details.
  */
 
-import React from 'react';
+import ClayButton from '@clayui/button';
+import ClayCard from '@clayui/card';
+import ClayProgressBar from '@clayui/progress-bar';
+import {createPortletURL, openSelectionModal} from 'frontend-js-web';
+import React, {Fragment, useState} from 'react';
 import ReactDOM from 'react-dom';
 
-export default function(elementId) {
-	ReactDOM.render(<span>Pokémon Portlet</span>, document.getElementById(elementId));
+const Pokemon = ({pokemon}) => {
+	return (
+		<ClayCard
+			className="flex-shrink-0 mw-100 mx-3 my-3 pokemon-card"
+			displayType="image"
+		>
+			<ClayCard.AspectRatio>
+				<img
+					className="aspect-ratio-item aspect-ratio-item-center-middle aspect-ratio-item-fluid aspect-ratio-item-flush p-1 p-lg-3"
+					src={pokemon.imageURL}
+				/>
+			</ClayCard.AspectRatio>
+			<ClayCard.Body>
+				<ClayCard.Description
+					className="text-uppercase"
+					displayType="title"
+				>
+					{pokemon.name}
+				</ClayCard.Description>
+
+				<div className="card-divider" />
+
+				<div className="card-text">
+					{pokemon.stats.map(({abbr, max, name, value}) => (
+						<div className="d-flex flex-wrap" key={name}>
+							<small
+								className="font-weight-bold pokemon-stat text-left text-uppercase"
+								title={name}
+							>
+								{abbr}
+							</small>
+							<ClayProgressBar
+								className="flex-grow-1 flex-wrap"
+								value={(value * 100) / max}
+							>
+								<div className="pokemon-progress-text">
+									{value}/{max}
+								</div>
+							</ClayProgressBar>
+						</div>
+					))}
+				</div>
+			</ClayCard.Body>
+		</ClayCard>
+	);
+};
+const PokemonSelector = ({eventName, itemSelectorURL}) => {
+	const [selectedItem, setSelectedItem] = useState();
+
+	return (
+		<Fragment>
+			<ClayButton
+				onClick={() =>
+					openSelectionModal({
+						onSelect: ({value}) =>
+							setSelectedItem(JSON.parse(value)),
+						selectEventName: eventName,
+						title: Liferay.Language.get('select-a-pokemon'),
+						url: itemSelectorURL,
+					})
+				}
+			>
+				{Liferay.Language.get('select-a-pokemon')}
+			</ClayButton>
+
+			<div className="align-items-center d-flex flex-wrap justify-content-center mt-2">
+				{selectedItem && <Pokemon pokemon={selectedItem} />}
+			</div>
+		</Fragment>
+	);
+};
+
+export default function (elementId, eventName, itemSelectorURL) {
+	ReactDOM.render(
+		<PokemonSelector
+			eventName={eventName}
+			itemSelectorURL={itemSelectorURL}
+		/>,
+		document.getElementById(elementId)
+	);
 }
