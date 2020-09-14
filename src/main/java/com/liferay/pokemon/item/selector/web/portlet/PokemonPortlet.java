6d692f4dc893cbc6e11adb655bdd57fd1cce489e
Alejandro Tardín <alejandro.tardin@liferay.com>
2020-09-14 17:13:39 +0200

    Step 0: Generating Liferay 7.3 module skeleton
    Command:
    
    blade create -t npm-react-portlet -v 7.3 -p com.liferay.pokemon.item.selector.web -c Pokemon pokemon-item-selector-web
    
    This is a simplified and cleaned up version of the actual blade output.

---
+++ src/main/java/com/liferay/pokemon/item/selector/web/portlet/PokemonPortlet.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.pokemon.item.selector.web.portlet;
+
+import com.liferay.frontend.js.loader.modules.extender.npm.NPMResolver;
+import com.liferay.pokemon.item.selector.web.constants.PokemonPortletKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import java.io.IOException;
+
+import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alejandro Tardín
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.fun",
+		"com.liferay.portlet.header-portlet-css=/css/index.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + PokemonPortletKeys.POKEMON,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class PokemonPortlet extends MVCPortlet {
+
+	@Override
+	public void doView(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws IOException, PortletException {
+
+		renderRequest.setAttribute(
+			"mainRequire",
+			_npmResolver.resolveModuleName("pokemon-item-selector-web") +
+				" as main");
+
+		super.doView(renderRequest, renderResponse);
+	}
+
+	@Reference
+	private NPMResolver _npmResolver;
+
+}
